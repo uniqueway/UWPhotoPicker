d0a9af76adf5ce6a41c560e47d383f5e2d2343ce
小六 <zhouxl72@126.com>
2016-05-13 16:32:48 +0800

    提示信息

--- UWPhotoPicker/UWPhotoPickerController.m
+++ UWPhotoPicker/UWPhotoPickerController.m
@@ -148,14 +148,14 @@ static NSInteger MAX_SELECTION_COUNT = INFINITY;
     if (!_dataManager.isSingleSelection) {
         if (_dataManager.countLocation == UWPhotoCountLocationBottom) {
             _segmentedControl.countOfImages = _dataManager.selectedCount;
-        }else {
-            self.navBar.count = self.dataManager.selectedCount;
             if (self.dataManager.selectedCount == 0 && !self.dataManager.isSingleMenu) {
                 UIAlertController *alert = [UIAlertController alertControllerWithTitle:nil message:@"照片为空，将会删除当前故事哦" preferredStyle:UIAlertControllerStyleAlert];
                 UIAlertAction *action = [UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleDestructive handler:nil];
                 [alert addAction:action];
                 [self presentViewController:alert animated:YES completion:nil];
             }
+        }else {
+            self.navBar.count = self.dataManager.selectedCount;
         }
     }
 }
